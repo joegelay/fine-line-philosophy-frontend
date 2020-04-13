47351a117b333f7a035df8ab5d8e6baf1859c506
joegelay <joegelay@gmail.com>
2020-04-13 14:07:32 -0600

    update syntax of custom select form

--- select.js
+++ select.js
@@ -1,45 +1,45 @@
-var x, i, j, selElmnt, a, b, c;
+var customSelectBox, i, j, pairOption, optionDiv, b, c;
 
-x = document.querySelector(".custom-select")
-  selElmnt = document.querySelector("#select-pair-form");
-  /* For each element, create a new DIV that will act as the selected item: */
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x.appendChild(a);
-  /* For each element, create a new DIV that will contain the option list: */
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < selElmnt.length; j++) {
-    /* For each option in the original select element,
-    create a new DIV that will act as an option item: */
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-        /* When an item is clicked, update the original select box,
-        and the selected item: */
-        var y, i, k, s, h;
-        x.classList.add('hide')
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < s.length; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            for (k = 0; k < y.length; k++) {
-              y[k].removeAttribute("class");
-            }
-            this.setAttribute("class", "same-as-selected");
-            break;
+customSelectBox = document.querySelector(".custom-select")
+pairOption = document.querySelector("#select-pair-form");
+/* For each element, create a new DIV that will act as the selected item: */
+optionDiv = document.createElement("DIV");
+optionDiv.setAttribute("class", "select-selected");
+optionDiv.innerHTML = pairOption.options[pairOption.selectedIndex].innerHTML;
+customSelectBox.appendChild(optionDiv);
+/* For each element, create a new DIV that will contain the option list: */
+b = document.createElement("DIV");
+b.setAttribute("class", "select-items select-hide");
+for (j = 1; j < pairOption.length; j++) {
+  /* For each option in the original select element,
+  create a new DIV that will act as an option item: */
+  c = document.createElement("DIV");
+  c.innerHTML = pairOption.options[j].innerHTML;
+  c.addEventListener("click", function(e) {
+      /* When an item is clicked, update the original select box,
+      and the selected item: */
+      var y, i, k, s, h;
+      customSelectBox.classList.add('hide')
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < s.length; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          for (k = 0; k < y.length; k++) {
+            y[k].removeAttribute("class");
           }
+          this.setAttribute("class", "same-as-selected");
+          break;
         }
-        h.click();
-    });
-    b.appendChild(c);
-  }
-  x.appendChild(b);
-  a.addEventListener("click", function(e) {
+      }
+      h.click();
+  });
+  b.appendChild(c);
+}
+  customSelectBox.appendChild(b);
+  optionDiv.addEventListener("click", function(e) {
     /* When the select box is clicked, close any other select boxes,
     and open/close the current select box: */
     e.stopPropagation();
